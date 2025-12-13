8a85ef1421ce5973599079a4e76309684ea895cb
youngwooKim <rladyddn258@gmail.com>
2025-12-13 12:32:44 +0900

    싱글톤 패턴

--- core/src/main/java/hello/core/order/AppConfig.java
+++ core/src/main/java/hello/core/order/AppConfig.java
@@ -17,7 +17,7 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     @Bean
-    public MemberService memberService(){
+    public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
@@ -27,12 +27,12 @@ public class AppConfig {
     }
 
     @Bean
-    public OrderService orderService(){
+    public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
 
     @Bean
-    public DiscountPolicy discountPolicy(){
+    public DiscountPolicy discountPolicy() {
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
---
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,47 @@
+package hello.core.singleton;
+
+import hello.core.member.MemberService;
+import hello.core.order.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer(){
+        AppConfig appConcig = new AppConfig();
+
+        //1. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConcig.memberService();
+
+        //2. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService2 = appConcig.memberService();
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        assertThat(memberService1).isNotSameAs(memberService2);
+
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singleTonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+
+    }
+
+
+
+}
